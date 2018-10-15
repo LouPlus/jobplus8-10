e2ad711cda3619e7f999f67357fb09ab4b9d6a75
nttzl <nttzl@qq.com>
2018-10-15 20:02:07 +0800

    fix #16 modify app.py __init__.py blueprint

--- nttzl/jobplus/app.py
+++ nttzl/jobplus/app.py
@@ -6,5 +6,17 @@ def create_app(config):
     app = FLASK(__name__)
     app.config.from_object(configs.get(config))
     db.init_app(app)
+    register_blueprints(app)
+
     return app
+
+def register_blueprints(app):
+    from .handlers import admin,user,front,company,job
+    app.register_blueprint(admin)
+    app.register_blueprint(user)
+    app.register_blueprint(front)
+    app.register_blueprint(company)
+    app.register_blueprint(job)
+
+
 
---
+++ nttzl/jobplus/handlers/__init__.py
@@ -0,0 +1,5 @@
+from .front import front
+from .company import company
+from .job import job
+from .admin import admin
+from .user import user
