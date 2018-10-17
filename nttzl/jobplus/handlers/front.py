6cbf36fc0f17d0ab93f5402e1052af6b3fc3dd8c
lisisharand <lisisharand@qq.com>
2018-10-17 16:46:51 +0800

    compayregister_complete

--- nttzl/jobplus/handlers/front.py
+++ nttzl/jobplus/handlers/front.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,render_template,url_for,redirect,flash
 from jobplus.forms import RegisterForm,LoginForm
+from jobplus.models import User,db,Job
 
 front = Blueprint('front',__name__)
 
@@ -11,9 +12,18 @@ def index():
 def login():
     return render_template('login.html')
 
-@front.route('/company-register')
-def company_register():
-    return render_template('register.html')
+@front.route('/companyregister',methods=['GET','POST'])
+def companyregister():
+    form = RegisterForm()
+    form.name.label = 'CompanyName'
+    if form.validate_on_submit():
+        company_user = form.create_user()
+        company_user.role = User.ROLE_COMPANY
+        db.session.add(company_user)
+        db.session.commit()
+        flash('register success','sucess')
+        return redirect(url_for('.login'))
+    return render_template('companyregister.html',form=form)
 
 @front.route('/userregister',methods=['GET','POST'])
 def userregister():
