716156c72c5d06a3080824620ee3c4239898a290
NEVERDIEME <770813889@qq.com>
2018-10-20 17:22:15 +0800

    modify admin.py

--- nttzl/jobplus/handlers/admin.py
+++ nttzl/jobplus/handlers/admin.py
@@ -1,4 +1,8 @@
 from flask import Blueprint
-
+from jobplus.decorators import admin_required
+from jobplus.models import
 admin = Blueprint('admin',__name__,url_prefix='/admin')
 
+
+@admin.route('/jobs')
+def
