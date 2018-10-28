e88723d7a09d2f1009c80c765d847acf2859d7b6
lingxiaojun <2760883684@qq.com>
2018-10-28 19:13:46 +0800

    implement company admin

--- nttzl/jobplus/models.py
+++ nttzl/jobplus/models.py
@@ -47,6 +47,12 @@ class User(Base, UserMixin):
 
     def __repr__(self):
         return '<User:{}>'.format(self.name)
+    
+    @property
+    def enable_jobs(self):
+        if not self.is_company:
+            raise AttributeError('User has no attribute enable_jobs')
+        return self.jobs.filter(Job.is_disable.is_(False))
 
     @property
     def password(self):
